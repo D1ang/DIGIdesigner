86bbf521170ce36243dcf45dff79320ec09fd7b5
Djang <django.heimgartner@gmail.com>
2020-02-26 14:56:23 +0100

    Removed canvas element from main.js and replaced with external svg added comments and cleaned up the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,31 @@
-(function() {
-	const canvas = document.getElementById('myCanvas');
-	const ctx = canvas.getContext('2d');
-	ctx.fillStyle = 'green';
-	ctx.save();
-	ctx.beginPath();
-	ctx.moveTo(227.1, 33.6);
-	ctx.bezierCurveTo(222.1, 24.8, 202.29999999999998, 16.5, 202.29999999999998, 16.5);
-	ctx.lineTo(167.59999999999997, 0.5);
-	ctx.bezierCurveTo(167.59999999999997, 0.5, 158.29999999999995, 7.4, 125.59999999999997, 7.7);
-	ctx.bezierCurveTo(93, 7.4, 89.2, 0.6, 89.2, 0.6);
-	ctx.lineTo(54.5, 16.6);
-	ctx.bezierCurveTo(54.5, 16.6, 34.7, 24.900000000000002, 29.7, 33.7);
-	ctx.bezierCurveTo(24.7, 42.5, 0.5, 88.2, 0.5, 88.2);
-	ctx.lineTo(35.7, 111.30000000000001);
-	ctx.lineTo(44.5, 101.4);
-	ctx.bezierCurveTo(44.5, 101.4, 58, 155.3, 58, 166.9);
-	ctx.bezierCurveTo(58, 178.5, 51.1, 276.5, 51.1, 276.5);
-	ctx.bezierCurveTo(51.1, 276.5, 58.800000000000004, 293.6, 128.2, 293.6);
-	ctx.lineTo(128.5, 293.6);
-	ctx.bezierCurveTo(197.9, 293.6, 205.6, 276.5, 205.6, 276.5);
-	ctx.bezierCurveTo(205.6, 276.5, 198.7, 178.5, 198.7, 166.9);
-	ctx.bezierCurveTo(198.7, 155.3, 212.2, 101.4, 212.2, 101.4);
-	ctx.lineTo(221, 111.30000000000001);
-	ctx.lineTo(256.2, 88.20000000000002);
-	ctx.bezierCurveTo(256.3, 88.1, 232.1, 42.4, 227.1, 33.6);
-	ctx.lineTo(227.1, 33.6);
-	ctx.closePath();
-	ctx.fill();
-	ctx.stroke();
-	ctx.restore();
-	draw.ctx();
-})();
+var canvas = new fabric.Canvas('canvas');
 
+//Grabs the Hex colour code from the option form and transforms the fill of the .svg file to the proper colour
 function colour() {
 	var colourOption = document.getElementById('colourSelect');
 	var i = colourOption.selectedIndex;
 
 	document.getElementById('shirt--inject-1').style.fill = document.getElementsByTagName('option')[i].value;
+}
 
-	//console.log(colourOption.options[i].text)
+//Image Loader to load a .png file on the canvas.
+var imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+	var reader = new FileReader();
+	reader.onload = function(event) {
+		var img = new Image();
+		img.onload = function() {
+			var imgInstance = new fabric.Image(img, {
+				scaleX: 0.08,
+				scaleY: 0.08,
+				left: 183,
+				top: 145
+			});
+			canvas.add(imgInstance);
+		};
+		img.src = event.target.result;
+	};
+	reader.readAsDataURL(e.target.files[0]);
 }
