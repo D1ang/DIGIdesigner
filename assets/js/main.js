2c22f5147bc6060cbb595c5ad4f08be53cf2f6a1
Djang <django.heimgartner@gmail.com>
2020-03-02 09:04:33 +0100

    Made the valuelist creation part of the function code, expanded the colourSelect function so it will create a svg based on the values of the valuelists

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,87 +33,72 @@ resizeCanvas();
 
 /* The following function is based on the Car selector from W3schools.com
 It wil listen on a change in the first valuelist and created an array that wil be used for a second valuelist based on the selection from the first */
-var garmentsAndBrands = {};
-garmentsAndBrands[''] = [ 'Select a brand' ];
-garmentsAndBrands['shirt'] = [ 'Select a brand', 'Nike', 'Asics' ];
-garmentsAndBrands['polo'] = [ 'Select a brand', 'Nike', 'Asics', 'Humel' ];
-garmentsAndBrands['hoody'] = [ 'Select a brand', 'Nike' ];
-
 function garmentSelect() {
-  var garmentList = document.getElementById('garment');
-  var brandsList = document.getElementById('brands');
-  
+	var garmentsAndBrands = {};
+	garmentsAndBrands[''] = [ 'Select a brand' ];
+	garmentsAndBrands['shirt'] = [ 'Select a brand', 'Nike', 'Asics' ];
+	garmentsAndBrands['polo'] = [ 'Select a brand', 'Nike', 'Asics', 'Humel' ];
+	garmentsAndBrands['hoody'] = [ 'Select a brand', 'Nike' ];
+
+	var garmentList = document.getElementById('garment');
+	var brandsList = document.getElementById('brands');
+
 	var selectGarment = garmentList.options[garmentList.selectedIndex].value;
 	while (brandsList.options.length) {
-    brandsList.remove(0);
+		brandsList.remove(0);
 	}
 	var garments = garmentsAndBrands[selectGarment];
 	if (garments) {
 		var i;
 		for (i = 0; i < garments.length; i++) {
 			var garment = new Option(garments[i], i);
-      brandsList.options.add(garment);
+			brandsList.options.add(garment);
 		}
 	}
 }
 
 /* The following function is based on the Car selector from W3schools.com
 It wil listen on a change in the first valuelist and created an array that wil be used for a second valuelist based on the selection from the first */
-var BrandsAndColours = {};
+function colourSelect() {
+	var BrandsAndColours = {};
 	BrandsAndColours[''] = [ 'Select a colour' ];
 	BrandsAndColours['Asics'] = [ 'Select a colour', 'Red', 'Blue' ];
 	BrandsAndColours['Nike'] = [ 'Select a colour', 'Yellow', 'Black', 'White' ];
-  BrandsAndColours['Hummel'] = [ 'Select a colour', 'Brown' ];
+	BrandsAndColours['Hummel'] = [ 'Select a colour', 'Brown' ];
 
-function colourSelect() {  
-  var brandList = document.getElementById('brands');
-  var colourList = document.getElementById('colour');
+	var brandList = document.getElementById('brands');
+	var colourList = document.getElementById('colour');
 
 	var selectBrand = brandList.options[brandList.selectedIndex].text;
 	while (colourList.options.length) {
-    colourList.remove(0);
+		colourList.remove(0);
 	}
 	var colours = BrandsAndColours[selectBrand];
 	if (colours) {
 		var i;
 		for (i = 0; i < colours.length; i++) {
 			var colour = new Option(colours[i], i);
-      colourList.options.add(colour);
+			colourList.options.add(colour);
 		}
 	}
+
+	var garmentList = document.getElementById('garment');
+	var selectGarment = garmentList.options[garmentList.selectedIndex].value;
+
+	document.getElementById('shirt').src = 'assets/svg/' + selectBrand.toLowerCase() + '-' + selectGarment + '.svg';
+	SVGInject(document.querySelector('img.injectable'));
 }
 
+//Grabs colour from the valuelist and transforms the fill of the .svg file to the proper colour.
+function activateColour() {
+	var colourOption = document.getElementById('colour');
+	var i = colourOption.selectedIndex;
 
-
-
-
-
+	document.getElementById('fill--inject-1').style.fill = document
+		.getElementById('colour')
+		.getElementsByTagName('option')[i].text;
+}
 
 function disableElement() {
 	document.getElementById('btn01').disabled = true;
 }
-
-
-
-
-
-function brandSelector() {
-	document.getElementById('shirt').src = 'assets/svg/asics-shirt.svg';
-	SVGInject(document.querySelector('img.injectable'));
-}
-
-
-
-
-
-
-
-//Grabs the Hex colour code from the option form and transforms the fill of the .svg file to the proper colour
-function activateColour() {
-  var colourOption = document.getElementById('colour'); 
-  var i = colourOption.selectedIndex;
-
-	document.getElementById('fill--inject-1').style.fill = document.getElementById('colour').getElementsByTagName('option')[i].text;
-}
-
-
