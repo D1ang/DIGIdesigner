4ab0d7c3a516c9a87b59b5fdcd044c1a03430270
Djang <django.heimgartner@gmail.com>
2020-03-07 16:10:10 +0100

    Improved code placement for better readability, created a function to disable fields on page load and created the function for gender selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,101 @@
+const canvas = new fabric.Canvas('canvas');
+
+const genderConfigurator = {};
+genderConfigurator['male'] = [ 'Select a garment', 'Shirt', 'Hoody', 'Longsleeve' ];
+genderConfigurator['female'] = [ 'Select a garment', 'Shirt', 'Hoody' ];
+
 const garmentsConfigurator = {};
-garmentsConfigurator[''] = ['Select a brand'];
-garmentsConfigurator['shirt'] = ['Select a brand', 'Asics', 'Craft', 'Nike'];
-garmentsConfigurator['polo'] = ['Select a brand', 'Asics', 'Craft'];
-garmentsConfigurator['hoody'] = ['Select a brand', 'Nike'];
+garmentsConfigurator[''] = [ 'Select a brand' ];
+garmentsConfigurator['Shirt'] = [ 'Select a brand', 'Asics', 'Craft', 'Nike' ];
+garmentsConfigurator['Hoody'] = [ 'Select a brand', 'Asics', 'Craft' ];
+garmentsConfigurator['Longsleeve;'] = [ 'Select a brand', 'Nike' ];
 
 const brandsConfigurator = {};
-brandsConfigurator[''] = ['Select a colour'];
-brandsConfigurator['Asics'] = ['Select a colour', 'Red', 'Blue', 'Purple', 'Gray'];
-brandsConfigurator['Craft'] = ['Select a colour', 'Yellow', 'Brown', 'White'];
-brandsConfigurator['Nike'] = ['Select a colour', 'Brown', 'Lime', 'Teal', 'Green', 'Maroon'];
+brandsConfigurator[''] = [ 'Select a colour' ];
+brandsConfigurator['Asics'] = [ 'Select a colour', 'Red', 'Blue', 'Purple', 'Gray' ];
+brandsConfigurator['Craft'] = [ 'Select a colour', 'Yellow', 'Brown', 'White' ];
+brandsConfigurator['Nike'] = [ 'Select a colour', 'Brown', 'Lime', 'Teal', 'Green', 'Maroon' ];
 
-const canvas = new fabric.Canvas('canvas');
+document.addEventListener('DOMContentLoaded', function disableFields() {
+  document.getElementById('garment').disabled = true;
+	document.getElementById('brands').disabled = true;
+	document.getElementById('colour').disabled = true;
+});
 
 /*------------------------Image loader to load some artwork on the canvas------------------------*/
 
-document.getElementById('images').addEventListener("change", function (e) {
-  let reader = new FileReader();
+document.getElementById('images').addEventListener(
+	'change',
+	function(e) {
+		let reader = new FileReader();
 
-  reader.onload = function (event) {
-    let imgObj = new Image();
-    imgObj.src = event.target.result;
+		reader.onload = function(event) {
+			let imgObj = new Image();
+			imgObj.src = event.target.result;
 
-    imgObj.onload = function () {
-      let img = new fabric.Image(imgObj);
+			imgObj.onload = function() {
+				let img = new fabric.Image(imgObj);
 
-      img.scaleToHeight(180);
-      img.scaleToWidth(180);
-      canvas.centerObject(img);
-      canvas.add(img);
-      canvas.renderAll();
-    };
-  };
+				img.scaleToHeight(180);
+				img.scaleToWidth(180);
+				canvas.centerObject(img);
+				canvas.add(img);
+				canvas.renderAll();
+			};
+		};
 
-  // If the user selected a image, load it
-  if (e.target.files[0]) {
-    reader.readAsDataURL(e.target.files[0]);
-  }
-}, false);
+		// If the user selected a image, load it
+		if (e.target.files[0]) {
+			reader.readAsDataURL(e.target.files[0]);
+		}
+	},
+	false
+);
 
 /*---------------------Bootstrap Custom Forms: filename appears after upload---------------------*/
 
-$('.custom-file-input').on('change', function () {
-  let fileName = $(this).val().split('\\').pop();
-  $(this).siblings('.custom-file-label').addClass('selected').html(fileName);
+$('.custom-file-input').on('change', function() {
+	let fileName = $(this).val().split('\\').pop();
+	$(this).siblings('.custom-file-label').addClass('selected').html(fileName);
 });
 
 /*--------------------------Resets the garment preview on field change--------------------------*/
 
 function resetGarments() {
-  document.getElementById('garments').src = 'assets/img/garment/empty-shirt.png'
+	document.getElementById('garments').src = 'assets/img/garment/empty-shirt.png';
+}
+
+/*-----------------------------------------Select Gender-----------------------------------------*/
+
+function selectGender() {
+	let radioButtons = document.getElementsByName('genders');
+	let selectedGender = '';
+	let garmentList = document.getElementById('garment');
+
+	for (let i = 0; i < radioButtons.length; i++) {
+		if (radioButtons[i].checked) {
+			selectedGender = radioButtons[i].value;
+			break;
+		}
+	}
+
+	while (garmentList.options.length) {
+		garmentList.remove(0);
+	}
+	let garments = genderConfigurator[selectedGender];
+	if (garments) {
+		for (let i = 0; i < garments.length; i++) {
+			let selectedGender = new Option(garments[i], i);
+			garmentList.options.add(selectedGender);
+		}
+	}
+
+	document.getElementById('brands').value = '';
+  document.getElementById('colour').value = '';
+
+  document.getElementById('garment').disabled = false;
+  document.getElementById('brands').disabled = true;
+  document.getElementById('colour').disabled = true;
 }
 
 /*------------------------------------Create brand valuelist-------------------------------------*/
@@ -58,24 +105,25 @@ function resetGarments() {
    wil be used for a second valuelist based on the selection from the first */
 
 function selectGarment() {
-  let garmentsElements = document.getElementById('garment');
-  let brandsList = document.getElementById('brands');
+	let garmentsElements = document.getElementById('garment');
+	let brandsList = document.getElementById('brands');
 
-  let garment = garmentsElements.options[garmentsElements.selectedIndex].value;
-  while (brandsList.options.length) {
-    brandsList.remove(0);
-  }
-  let garments = garmentsConfigurator[garment];
-  if (garments) {
-    for (let i = 0; i < garments.length; i++) {
-      let garment = new Option(garments[i], i);
-      brandsList.options.add(garment);
-    }
-  }
+	let garment = garmentsElements.options[garmentsElements.selectedIndex].text;
+	while (brandsList.options.length) {
+		brandsList.remove(0);
+	}
+	let garments = garmentsConfigurator[garment];
+	if (garments) {
+		for (let i = 0; i < garments.length; i++) {
+			let garment = new Option(garments[i], i);
+			brandsList.options.add(garment);
+		}
+	}
 
-  resetGarments();
+	document.getElementById('brands').disabled = false;
+	resetGarments();
 
-  document.getElementById('garments').src = 'assets/img/garment/' + 'empty-' + garment + '.png';
+	document.getElementById('garments').src = 'assets/img/garment/' + 'empty-' + garment + '.png';
 }
 
 /*------------------------------------Create colour valuelist------------------------------------*/
@@ -85,27 +133,28 @@ function selectGarment() {
    wil be used for a third valuelist based on the selection from the second */
 
 function selectBrand() {
-  let brandElements = document.getElementById('brands');
-  let colourList = document.getElementById('colour');
+	let brandElements = document.getElementById('brands');
+	let colourList = document.getElementById('colour');
 
-  let selectBrand = brandElements.options[brandElements.selectedIndex].text;
-  while (colourList.options.length) {
-    colourList.remove(0);
-  }
-  let colours = brandsConfigurator[selectBrand];
-  if (colours) {
-    for (let i = 0; i < colours.length; i++) {
-      let colour = new Option(colours[i], i);
-      colourList.options.add(colour);
-    }
-  }
+	let selectBrand = brandElements.options[brandElements.selectedIndex].text;
+	while (colourList.options.length) {
+		colourList.remove(0);
+	}
+	let colours = brandsConfigurator[selectBrand];
+	if (colours) {
+		for (let i = 0; i < colours.length; i++) {
+			let colour = new Option(colours[i], i);
+			colourList.options.add(colour);
+		}
+	}
 
-  resetGarments();
+	document.getElementById('colour').disabled = false;
+	resetGarments();
 
-  let garmentsElements = document.getElementById('garment');
-  let garment = garmentsElements.options[garmentsElements.selectedIndex].value;
+	let garmentsElements = document.getElementById('garment');
+	let garment = garmentsElements.options[garmentsElements.selectedIndex].value;
 
-  document.getElementById('garments').src = 'assets/img/garment/' + 'empty-' + garment + '.png';
+	document.getElementById('garments').src = 'assets/img/garment/' + 'empty-' + garment + '.png';
 }
 
 /*---------------------------------------Colour activator----------------------------------------*/
@@ -114,19 +163,20 @@ function selectBrand() {
   of the .div begind the png file to the proper colour. */
 
 function selectColour() {
-  let colourOption = document.getElementById('colour');
-  let i = colourOption.selectedIndex;
-  document.getElementById('garment-div').style.backgroundColor = document.getElementById('colour').getElementsByTagName('option')[i].text;
+	let colourOption = document.getElementById('colour');
+	let i = colourOption.selectedIndex;
+	document.getElementById('garment-div').style.backgroundColor = document
+		.getElementById('colour')
+		.getElementsByTagName('option')[i].text;
 }
 
 /*----------------------------------------Download Image-----------------------------------------*/
 
 function downloadImage() {
-    domtoimage.toJpeg(document.getElementById('garment-div'), { quality: 0.95 })
-    .then(function (dataUrl) {
-        let link = document.createElement('a');
-        link.download = 'sample.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
+	domtoimage.toJpeg(document.getElementById('garment-div'), { quality: 0.95 }).then(function(dataUrl) {
+		let link = document.createElement('a');
+		link.download = 'sample.jpeg';
+		link.href = dataUrl;
+		link.click();
+	});
 }
